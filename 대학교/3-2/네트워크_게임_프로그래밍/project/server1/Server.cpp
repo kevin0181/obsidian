5effbd2f8f2a20ccbc6031803697dfa0a916d524
youngbeen you <59413865+kevin0181@users.noreply.github.com>
2025-09-05 12:00:16 +0900

    vault backup: 2025-09-05 12:00:15

--- 대학교/3-2/네트워크_게임_프로그래밍/project/server1/Server.cpp
+++ 대학교/3-2/네트워크_게임_프로그래밍/project/server1/Server.cpp
@@ -1,3 +1,18 @@
+#include <winsock2.h>
+
+#define SERVERPORT 9000
+#define BUFSIZE 512
 int main() {
+	int retval;
+	
+	//扩加 檬扁拳
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+		return 1;
+	}
+
+	//家南 积己
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listen_sock == INVALID_SOCKET) err_quit("socket()");
 
 }
