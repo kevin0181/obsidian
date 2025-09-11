4ec82e3ed642198d433edb42d480d218a81070eb
youngbeen you <59413865+kevin0181@users.noreply.github.com>
2025-09-11 10:48:10 +0900

    vault backup: 2025-09-11 10:48:09

--- 대학교/3-2/네트워크_게임_프로그래밍/server_project/ch2/main.cpp
+++ 대학교/3-2/네트워크_게임_프로그래밍/server_project/ch2/main.cpp
@@ -1,12 +1,23 @@
 #include "../Common.h"
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+	BYTE low = 2; // 0x02
+	BYTE high = 2; // 0x02
+
+	WORD version = (WORD)high << 8 | low; // 왼쪽으로 두칸 밀어주고
+
 	// 윈속 초기화
 	WSADATA wsa;
-	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	if (WSAStartup(version, &wsa) != 0)
 		return 1;
 	printf("[알림] 윈속 초기화 성공\n");
+
+	std::cout << "wVersion : " << std::hex << wsa.wVersion << std::endl;
+	std::cout << "wHighVersion : " << wsa.wHighVersion << std::endl;
+	std::cout << "szDescription : " << std::dec << wsa.szDescription << std::endl;
+	std::cout << "szSystemStatus : " << wsa.szSystemStatus << std::endl;
 
 	// 소켓 생성
 	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
