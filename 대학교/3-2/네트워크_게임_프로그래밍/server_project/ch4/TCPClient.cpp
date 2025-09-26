cc4c463cf070fc73ccd4a20e60e4d872426bf751
youngbeen you <59413865+kevin0181@users.noreply.github.com>
2025-09-26 11:48:36 +0900

    vault backup: 2025-09-26 11:48:36

--- 대학교/3-2/네트워크_게임_프로그래밍/server_project/ch4/TCPClient.cpp
+++ 대학교/3-2/네트워크_게임_프로그래밍/server_project/ch4/TCPClient.cpp
@@ -1,6 +1,7 @@
 #include "../Common.h"
 
-char* SERVERIP = (char*)"127.0.0.1";
+//char* SERVERIP = (char*)"127.0.0.1";
+char* SERVERIP = (char*)"10.20.11.15";
 #define SERVERPORT 9000
 #define BUFSIZE    512
 
